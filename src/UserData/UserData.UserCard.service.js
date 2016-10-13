b2eef5d8d87e650b36dfb69d81d431bc1298475e
AMashoshyna <a.mashoshyna@gmail.com>
2016-10-13 22:01:03 +0300

    link correction

--- src/UserData/UserData.UserCard.service.js
+++ src/UserData/UserData.UserCard.service.js
@@ -10,22 +10,22 @@ function UserCardService($http) {
 	var service = this;
 
 	this.getUserData = function() {
-		return $http.get('http://test-api.javascript.ru/v1/amashoshyna/users')
+		return $http.get('//test-api.javascript.ru/v1/amashoshyna/users')
 		.then((response) => response.data);
 	};
 
 	this.addUser = (newUser) => {
-		return $http.post('http://test-api.javascript.ru/v1/amashoshyna/users', newUser)
+		return $http.post('//test-api.javascript.ru/v1/amashoshyna/users', newUser)
 		.then((response) => response.data);
 	};
 
 	this.removeUser = function(user) {
-		return $http.delete('http://test-api.javascript.ru/v1/amashoshyna/users/'+ user._id)
+		return $http.delete('//test-api.javascript.ru/v1/amashoshyna/users/'+ user._id)
 		.then((response) => response.data);
 	};
 
 	this.getRandomAvatar = function() {
-		return $http.get('http://api.randomuser.me/?format=json').then((response) => response.data)
+		return $http.get('//api.randomuser.me/?format=json').then((response) => response.data)
 	}
 
 };
