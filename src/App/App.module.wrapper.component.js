8f641eb401e6466f07fa872e35e0147dec96bb5f
AMashoshyna <a.mashoshyna@gmail.com>
2016-10-17 21:19:16 +0300

    minor view changes

--- src/App/App.module.wrapper.component.js
+++ src/App/App.module.wrapper.component.js
@@ -6,7 +6,7 @@
 		template: 
 		`<div class="container">
 	   <ul class="nav nav-pills">
-       <li><a ui-sref="users" ng-class="{active: $state.includes('users')}">USERS</a></li>
+       <li><a ui-sref="users" ng-class="{active: $state.includes('users')}">CONTACTS</a></li>
        <li><a ui-sref="mails.inbox" ng-class="{active: $state.includes('mails')}">MAILBOX</a></li>
 	   </ul>    
        <ui-view></ui-view>
